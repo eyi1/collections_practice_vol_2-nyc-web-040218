f09f63e50624fc9d85b0ad3f05c3c4a077376d7d
eyi1 <ej7729@gmail.com>
2018-03-28 21:55:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
-# your code goes here
+def begins_with_r (tools)
+  tools.all? do |word|
+  if word[0] == "r"
+    true
+  else
+    false
+  end
+end
+
+end
+
+def contain_a (list)
+list.select {|word| word.include?("a")}
+
+end
+
+def first_wa (list)
+  list.find do |word|
+    word[0..1] == "wa"
+  end
+end
+
+
+def remove_non_strings (list)
+list.delete_if do |word|
+  word.class != String
+end
+end
